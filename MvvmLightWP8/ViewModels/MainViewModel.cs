a0760df9bb08363dc4693aa605d2814e2ad955e2
Wassim AZIRAR <wassim.azirar@gmail.com>
2014-12-03 23:17:11 +0100

    Created a Windows Phone 8 App and added some libraries

---
+++ MvvmLightWP8/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MvvmLightWP8.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
